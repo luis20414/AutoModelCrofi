b292fe074b739e04a0a0a6c193ee04489c5a7334
gmsebastian <sgmartes19@comunidad.unam.mx>
2025-04-08 18:27:08 -0600

    Mejoras de practicidad para uso inalámbrico

---
+++ ros2_ws/launch/AutoModel.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='webcam',
+            executable='webcam',
+            name='webcam_node'
+        ),
+        Node(
+            package='webcam_recorted',
+            executable='webcam_recorted',
+            name='webcam_recorted_node'
+        ),
+        Node(
+            package='lane_detector',
+            executable='lane_detector',
+            name='lane_detector_node'
+        ),
+        Node(
+            package='lane_angle',
+            executable='lane_angle',
+            name='lane_angle_node'
+        ),
+        Node(
+            package='servo',
+            executable='servo',
+            name='servo_node'
+        )
+    ])
